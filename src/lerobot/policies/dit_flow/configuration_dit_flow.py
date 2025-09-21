46540f300e274054a7ad6ab4267eee58ba66543c
Nirmal Kumar Ramadoss <nirmal.kumar.3.14159@gmail.com>
2025-09-21 13:18:18 +0200

    Assign better default values

--- src/lerobot/policies/dit_flow/configuration_dit_flow.py
+++ src/lerobot/policies/dit_flow/configuration_dit_flow.py
@@ -112,13 +112,13 @@ class DiTFlowConfig(PreTrainedConfig):
 
     # Architecture / modeling.
     # Vision backbone.
-    vision_backbone: str = "resnet34"
-    crop_shape: tuple[int, int] | None = (224, 224)
+    vision_backbone: str = "resnet18"
+    crop_shape: tuple[int, int] | None = None
     crop_is_random: bool = False
     pretrained_backbone_weights: str | None = None
     use_group_norm: bool = True
     spatial_softmax_num_keypoints: int = 32
-    use_separate_rgb_encoder_per_camera: bool = True
+    use_separate_rgb_encoder_per_camera: bool = False
 
     # Diffusion Transformer (DiT) parameters.
     frequency_embedding_dim: int = 256
